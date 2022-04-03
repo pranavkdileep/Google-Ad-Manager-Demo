690dd251aac71d42611fe56ed3994c259cb596a2
Pranav Dileep <91619846+pranavkdileep@users.noreply.github.com>
2022-04-03 08:53:58 +0530

    Update Tutorial.java

--- Tutorial.java
+++ Tutorial.java
@@ -22,6 +22,9 @@ private void loadBannerAd() {
 <string name="banner_id_admob">/6499/example/banner</string>
 
 <uses-permission android:name="android.permission.INTERNET" />
+<meta-data
+            android:name="com.google.android.gms.ads.APPLICATION_ID"
+            android:value="ca-app-pub-3940256099942544~3347511713"/>
 
 
 <com.google.android.gms.ads.admanager.AdManagerAdView
